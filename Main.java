cb896f9536b49b8b5556eba46cede9101f2884e3
amruthaaddanki25 <amruthaaddanki25@gmail.com>
2023-06-24 23:07:32 +0530

    Added another two numbers for addition.

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ public class Main
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 225, n2 = 115, sum;
+int n3 =25, n4 =07;
 sum = n1 + n2;  
+int sum1 = n3+n4;
 System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of numbers is: "+sum1);  
 }  
 }  
